e9abf2f67b5de60df21b828c8ae07f8f0cf80d68
AlexLemoine <alexandra.claire1409@gmail.com>
2023-05-24 16:13:43 +0200

    page boulangerie ok

--- views/boulangerie.php
+++ views/boulangerie.php
@@ -3,44 +3,63 @@
 	<div class="MainContent-titleWrap">
 		<h1 class="MainContent-title">Notre boulangerie</h1>
 	</div>
-	
-	<section id="gamme">
 
-		<p><?php foreach ($presentation as $item) {
+
+	<!--	VIDEO ENTREPRISE A VENIR  -->
+	<img class="Boulangerie-media" src="assets/img/painsHeader.jpg" alt="Image de pains en cuisson">
+	
+	<section id="gamme" class="Boulangerie-gamme">
+
+		<p class="Boulangerie-gamme-text"><?php foreach ($presentation as $item) {
 				if ($item->getTheme() === "gamme") {
 					echo $item->getText();
 				}
 			}; ?>
 		</p>
 		
-		<!--	VIDEO ENTREPRISE A VENIR  -->
-		<img src="assets/img/painsHeader.jpg" alt="Image de pains en cuisson">
-		
 	</section>
-	
-	
-	<section id="produits">
 
-		<p><?php foreach ($presentation as $item) {
+	<div class="Boulangerie-encart">
+		<p class="Boulangerie-encart-tag">Des produits sains</p>
+		<p class="Boulangerie-encart-tag">100% Bio</p>
+	</div>
+	
+	
+	<section id="produits" class="Boulangerie-produits">
+		
+		<h2 class="Boulangerie-produits-title">Nos produits</h2>
+
+		<p class="Boulangerie-produits-text"><?php foreach ($presentation as $item) {
 				if ($item->getTheme() === "produits") {
 					echo $item->getText();
 				}
 			}; ?>
 		</p>
 
+		<img class="Boulangerie-produits-img" src="assets/img/champs_de_blé.png" alt="champs de blé">
 		
 		<a class="CTA" href="?page=<?= PAGE_PRODUCTS ?>">Découvrir nos produits</a>
+		
 	</section>
 	
+	<div class="Boulangerie-encart">
+		<p class="Boulangerie-encart-tag">Développement durable</p>
+		<p class="Boulangerie-encart-tag">Producteurs locaux</p>
+	</div>
 	
-	<section id="partenaires">
+	
+	<section id="partenaires" class="Boulangerie-partenaires">
 
-		<p><?php foreach ($presentation as $item) {
+		<h2 class="Boulangerie-partenaires-title">Nos partenaires</h2>
+
+		<p class="Boulangerie-partenaires-text"><?php foreach ($presentation as $item) {
 				if ($item->getTheme() === "partenaires") {
 					echo $item->getText();
 				}
 			}; ?>
 		</p>
+
+		<img class="Boulangerie-partenaires-img" src="assets/img/field.jpg" alt="champs de blé">
 		
 		<a class="CTA" href="#">Découvrir nos partenaires</a>
 	</section>
