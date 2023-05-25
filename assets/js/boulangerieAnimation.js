185599c8ab49fb6d397b60d2dd69a444599ffaa7
AlexLemoine <alexandra.claire1409@gmail.com>
2023-05-25 12:50:38 +0200

    animation page boulangerie ok

--- assets/js/boulangerieAnimation.js
+++ assets/js/boulangerieAnimation.js
@@ -1,13 +1,21 @@
+const tags = document.querySelectorAll('.Boulangerie-encart-tag');
+
 window.addEventListener('scroll', function() {
-    const tags = document.querySelectorAll('.Boulangerie-encart-tag');
     const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
 
     for (let i = 0; i < tags.length; i++) {
         const tag = tags[i];
-        const tagTop = tag.getBoundingClientRect().top;
+        const tagTop = tag.offsetTop;
+        const tagHeight = tag.offsetHeight;
 
-        if (tagTop < windowHeight) {
+        const tagVisibleStart = tagTop + tagHeight * 0.4; // 40% de visibilité
+        const tagVisibleEnd = tagTop + tagHeight - tagHeight * 0.4; // 40% de visibilité
+
+        if (scrollPosition + windowHeight > tagVisibleStart && scrollPosition < tagVisibleEnd) {
             tag.classList.add('visible');
+        } else {
+            tag.classList.remove('visible');
         }
     }
 });
