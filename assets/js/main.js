9cdb615a1a47d0aedd48642a7db83a028220ee5b
AlexLemoine <alexandra.claire1409@gmail.com>
2023-05-25 15:13:20 +0200

    animation bouton topBack ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+// *** MENU BURGER ***
 function toggleClassOpened(element){
     element.classList.toggle('opened');
 }
@@ -10,3 +10,30 @@ burgerMenu.addEventListener('click', function() {
     toggleClassOpened(burgerMenu);
     toggleClassOpened(burgerMenuLinks);
 });
+
+
+// *** BOUTON HAUT DE PAGE ***
+// Récupérer l'élément <main>
+const mainElement = document.querySelector('main');
+
+// Calculer la hauteur du <main>
+const mainHeight = mainElement.offsetHeight;
+
+// Calculer le point de déclenchement (20% de la hauteur du <main>)
+const triggerHeight = mainHeight * 0.2;
+
+// Fonction pour afficher ou masquer le bouton en fonction de la position de défilement
+function toggleBackToTopButton() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > triggerHeight) {
+        // Afficher le bouton "Haut de page"
+        document.querySelector('.layout.MainContent-topBack').style.display = 'block';
+    } else {
+        // Masquer le bouton "Haut de page"
+        document.querySelector('.layout.MainContent-topBack').style.display = 'none';
+    }
+}
+
+// Écouter l'événement de défilement
+window.addEventListener('scroll', toggleBackToTopButton);
